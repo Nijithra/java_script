4e88f921c3e919a7a522e727f16cf44c77affbda
Nijithra <nijithragk@gmail.com>
2024-01-22 10:33:29 +0530

    This is my commit

--- operators.js
+++ operators.js
@@ -12,9 +12,26 @@ console.log("Arithmetic Operator Subtion:",aA-bA)
 console.log("Arithmetic Operator Multiplication:",aA*bA)
 console.log("Arithmetic Operator Division:",aA/bA)
 console.log("Arithmetic Operator Modulo Division:",aA%bA)
+console.log("Arithmetic Operator Post Increment:",aA++)
+console.log("Arithmetic Operator Pre Increment:",++aA)
+console.log("Arithmetic Operator Post Decrement:",aA--)
+console.log("Arithmetic Operator Pre Decrement:",--aA)
 console.log("\n")
 
 //Logic Operator --> &&, ||, !
 console.log("Arithmetic and Operator:",aA&&bA)
 console.log("Arithmetic or Operator:",aA||bA)
 console.log("Arithmetic not Operator:",!bA)
+console.log("\n")
+
+//relational operator
+console.log("Relational Operator > is:",aA>bA)
+console.log("Relational Operator >= is:",aA>=bA)
+console.log("Relational Operator < is:",aA<bA)
+console.log("Relational Operator <= is:",aA<=bA)
+console.log("Relational Operator == is:",aA==bA)
+console.log("Relational Operator != is:",aA!=bA)
+
+//Objects
+//how will you declare objects.
+
