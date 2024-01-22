37ec16682278335bcbd2eef0f194e3919fb88a18
Nijithra <nijithragk@gmail.com>
2024-01-22 10:31:55 +0530

    This is my commit

---
+++ variables.js
@@ -0,0 +1,8 @@
+a = 'ab'
+console.log("1",a)
+var b = 'var'
+console.log("This variable belongs to var type value is var b ='var':",b)
+let c = 'let'
+console.log("This variable belongs to let, let c='let' type:",c)
+const d = 100
+console.log("Constant Variable const d =",d)
